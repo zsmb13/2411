55b2a66a58fce4357af03d775be8b5ada4350d74
Márton Braun <braunmarci@gmail.com>
2020-12-01 09:44:10 +0100

    Fix test (??)

--- app/src/test/java/com/example/issue2411/IssueTest.kt
+++ app/src/test/java/com/example/issue2411/IssueTest.kt
@@ -4,8 +4,11 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.yield
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,8 +19,10 @@ class IssueTest {
     val testCoroutineRule = TestCoroutineRule()
 
     @Test
-    fun `test`() = runBlockingTest {
+    fun `test`() = testCoroutineRule.dispatcher.runBlockingTest {
         DataController(testCoroutineRule.dispatcher).let { controller ->
+            delay(1) // could also be yield()
+
             // should start null if combine runs after stateIn sets 0
             val initialValue = controller.combinedFlow.value
             println("Initial $initialValue")
